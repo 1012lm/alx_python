59415b54cc1d00bb8c3adfd5240bbfbd243c3bac
1012lm <lm1012smile@gmail.com>
2023-07-15 12:57:44 -0800

    edit

--- python-hello_world/1-edges.py
+++ python-hello_world/1-edges.py
@@ -1,8 +1,8 @@
 word = input("enter the word : ")
 
-first  = word[:3]
-print("First 3 letters: " , first)
-last= word[-2:]
-print("Last 2 letters: " , last)
+first = word[:3]
+print("First 3 letters: ",first)
+last = word[-2:]
+print("Last 2 letters: ",last)
 middle = word[1:-1]
-print("Middle word: " , middle)
+print("Middle word: ",middle)

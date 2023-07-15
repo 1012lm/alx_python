239554aabb7fb2491d074a926282ee448b4e45a4
1012lm <lm1012smile@gmail.com>
2023-07-15 13:30:21 -0800

    edit

--- python-hello_world/2-positive_or_negative.py
+++ python-hello_world/2-positive_or_negative.py
@@ -1,10 +1,7 @@
-# Importing the necessary module
 import random
 
-# Generating a random number between -10 and 10 (inclusive)
-number = random.randint(-10, 10)
+number = random.randint(-100, 100)
 
-# Checking whether the number is positive, negative, or zero
 if number > 0:
     print(number, "is positive")
 elif number == 0:

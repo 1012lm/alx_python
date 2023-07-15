ae14d39e2bfccead801297680a501762af1a4136
1012lm <lm1012smile@gmail.com>
2023-07-15 11:54:59 -0800

    edit

--- python-hello_world/1-edges.py
+++ python-hello_world/1-edges.py
@@ -1,9 +1,8 @@
-word = "holberton"
+word = "Hello"
 
-word_first_3 = word[:3]
-word_last_2 = word[-2:]
-middle_word = word[1:-1]
-
+word_first_3 = word[:3];
 print(word_first_3)
+word_last_2 = word[-2:]; 
 print(word_last_2)
+middle_word = word[1:-1]; 
 print(middle_word)

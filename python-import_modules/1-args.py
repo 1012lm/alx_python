97ae00f1376b3cadcc4a470c146d32a258fcce41
1012lm <lm1012smile@gmail.com>
2023-07-25 04:32:49 -0800

    sdcghsdf

--- python-import_modules/1-args.py
+++ python-import_modules/1-args.py
@@ -3,11 +3,11 @@ import sys
 
 args = sys.argv[1:]
 num_args = len(args)
-
-if num_args == 0:
-    print("0 arguments.")
-else:
-    print("{} arguments: ".format(num_args))
+if __name__ == "__main__":
+    if num_args == 0:
+        print("0 arguments.")
+    else:
+        print("{} arguments: ".format(num_args))
    
-    for i, arg in enumerate(args):
-        print("{}: {}".format(i+1, arg))
+        for i, arg in enumerate(args):
+            print("{}: {}".format(i+1, arg))

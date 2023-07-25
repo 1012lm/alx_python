e7295280abdaf66c109318cabc25b5f973c4511e
1012lm <lm1012smile@gmail.com>
2023-07-25 03:59:12 -0800

    bi

--- python-import_modules/0-add.py
+++ python-import_modules/0-add.py
@@ -1,7 +1,7 @@
 from add_0 import add
 a = 1
 b = 2
-result = add(a,b)
+result = add(a, b)
 
 if __name__ == "__main__":
-    print( "{} + {} = {}" .format(a, b, result))
+    print("{} + {} = {}" .format(a, b, result))

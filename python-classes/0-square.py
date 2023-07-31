8af5804082b641f186e71e4ff10ba25686fc22ed
1012lm <lm1012smile@gmail.com>
2023-07-31 05:43:30 -0800

    task 1

--- python-classes/0-square.py
+++ python-classes/0-square.py
@@ -1,6 +1,10 @@
 class Square:
     """
-    A class that defines a square by its size.
+    This is a class that defines a square by its size.
+
+    Attributes:
+        __size (int): The size of the square.
+
     """
 
     def __init__(self, size):
@@ -8,6 +12,7 @@ class Square:
         Initializes a new instance of the Square class with a size parameter.
 
         Args:
-            size: The size of the square.
+            size (int): The size of the square.
+
         """
         self.__size = size

92465903f519718711c2165e7197e75ce24f6a33
1012lm <lm1012smile@gmail.com>
2023-08-07 12:29:53 -0800

    new project

--- python-inheritance/0-is_same_class.py
+++ python-inheritance/0-is_same_class.py
@@ -1,2 +1,20 @@
+"""
+This module defines a class called Square, which represents a square with a given size.
+
+Attributes:
+    __size (int): The size of the square.
+
+"""
+
 def is_same_class(obj, a_class):
+    """
+    This function checks if an object is an instance of a given class.
+
+    Parameters:
+        obj (object): The object to check.
+        a_class (class): The class to compare against.
+
+    Returns:
+        bool: True if the object is an instance of the given class, False otherwise.
+    """
     return type(obj) is a_class

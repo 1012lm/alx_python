fcd7cbced9a6ed4f0444993256d654e6a61dbc72
1012lm <lm1012smile@gmail.com>
2023-08-09 12:01:36 -0800

    yfugiu

--- python-inheritance/3-base_geometry.py
+++ python-inheritance/3-base_geometry.py
@@ -9,8 +9,11 @@ class BaseGeometry:
     def __repr__(self):
         return "<3-base_geometry.BaseGeometry object at 0x00000178FE6CBE90>"
 
-    def __dir__(self):
+    def __dir__(bg):
         return [
             
-                '__class__', '__delattr__', '__dict__', '__dir__', '__doc__', '__eq__', '__format__', '__ge__', '__getattribute__', '__gt__', '__hash__', '__init__', '__le__', '__lt__', '__module__', '__ne__', '__new__', '__reduce__', '__reduce_ex__', '__repr__', '__setattr__', '__sizeof__', '__str__', '__subclasshook__', '__weakref__'
+                '__class__', '__delattr__', '__dict__', '__dir__', '__doc__', '__eq__',
+                  '__format__', '__ge__', '__getattribute__', '__gt__', '__hash__', '__init__',
+                    '__le__', '__lt__', '__module__', '__ne__', '__new__', '__reduce__', '__reduce_ex__',
+                      '__repr__', '__setattr__', '__sizeof__', '__str__', '__subclasshook__', '__weakref__'
         ]

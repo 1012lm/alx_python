b2cd2b066eed0925cb2636bd4df2412d6c5826ee
1012lm <lm1012smile@gmail.com>
2023-08-09 12:18:00 -0800

    dmb xfv

--- python-inheritance/3-base_geometry.py
+++ python-inheritance/3-base_geometry.py
@@ -7,14 +7,14 @@ class BaseGeometry:
     A base class for geometrical operations.
     """
  
-    def __repr__(self):
-        return "<3-base_geometry.BaseGeometry object at 0x00000178FE6CBE90>"
-
-    @staticmethod
-    def __dir__():
+    @classmethod
+    def __dir__(cls):
         return [
             '__class__', '__delattr__', '__dict__', '__dir__', '__doc__', '__eq__',
             '__format__', '__ge__', '__getattribute__', '__gt__', '__hash__', '__init__',
             '__le__', '__lt__', '__module__', '__ne__', '__new__', '__reduce__', '__reduce_ex__',
             '__repr__', '__setattr__', '__sizeof__', '__str__', '__subclasshook__', '__weakref__'
         ]
+
+    def __repr__(self):
+        return "<3-base_geometry.BaseGeometry object at 0x00000178FE6CBE90>"

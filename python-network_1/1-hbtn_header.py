b74d8befe8ca205aa23d28f7233c8c5706e549cf
1012lm <lm1012smile@gmail.com>
2023-08-10 00:26:16 -0800

    , l.cd c

---
+++ python-network_1/1-hbtn_header.py
@@ -0,0 +1,12 @@
+"""
+hjsv;v  lufg;v fv
+"""
+import requests
+import sys
+
+url = sys.argv[1]
+
+response = requests.get(url)
+request_id = response.headers.get('X-Request-Id')
+if __name__ == "__main__":
+    print(request_id)

c45306669424865d3351505d65edaf59978d197c
1012lm <lm1012smile@gmail.com>
2023-08-09 22:30:42 -0800

    nbadiusf

--- python-inheritance/6-rectangle.py
+++ python-inheritance/6-rectangle.py
@@ -46,13 +46,6 @@ class Rectangle(BaseGeometry):
         __width (int): The width of the rectangle.
         __height (int): The height of the rectangle.
     """
-    def __dir__(Rectangle):
-        return [
-          '__class__', '__delattr__', '__dict__', '__dir__', '__doc__', '__eq__',
-            '__format__', '__ge__', '__getattribute__', '__gt__', '__hash__', '__init__',
-              '__le__', '__lt__', '__module__', '__ne__', '__new__', '__reduce__', '__reduce_ex__',
-                '__repr__', '__setattr__', '__sizeof__', '__str__', '__subclasshook__', '__weakref__', 'area', 'integer_validator'
-        ]
     def __init__(self, width, height):
         """
         Initialize a rectangle with the given width and height.
@@ -74,3 +67,18 @@ class Rectangle(BaseGeometry):
             str: The string representation of the rectangle in the format "[Rectangle] width/height".
         """
         return "[Rectangle] {}/{}".format(self.__width, self.__height)
+
+    def __dir__(self):
+        """
+        Return a list of attributes and methods of the Rectangle class.
+
+        Returns:
+            list: A list of attributes and methods.
+        """
+        return [
+            '__class__', '__delattr__', '__dict__', '__dir__', '__doc__', '__eq__',
+            '__format__', '__ge__', '__getattribute__', '__gt__', '__hash__', '__init__',
+            '__le__', '__lt__', '__module__', '__ne__', '__new__', '__reduce__', '__reduce_ex__',
+            '__repr__', '__setattr__', '__sizeof__', '__str__', '__subclasshook__', '__weakref__',
+            'area', 'integer_validator'
+        ]

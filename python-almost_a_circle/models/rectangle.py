2ea3320678ef70cdfee2eae6998927124fa21cca
1012lm <lm1012smile@gmail.com>
2023-08-10 00:03:03 -0800

    nbcewtgn

--- python-almost_a_circle/models/rectangle.py
+++ python-almost_a_circle/models/rectangle.py
@@ -6,97 +6,116 @@
 from models.base import Base
 
 class Rectangle(Base):
-    """
+     """
     nbd gb ;wirobgf kjhgvd
     """
-    def __init__(self, width, height, x=0, y=0, id=None):
-        super().__init__(id)
-        self.width = width
-        self.height = height
-        self.x = x
-        self.y = y
-
-    @property
-    def width(self):
+     def __init__(self, width, height, x=0, y=0, id=None):
+         """
+            nbd gb ;wirobgf kjhgvd
         """
-    nbd gb ;wirobgf kjhgvd
-    """
-        return self.__width
+         super().__init__(id)
+         self.width = width
+         self.height = height
+         self.x = x
+         self.y = y
 
-    @width.setter
-    def width(self, value):
+     @property
+     def width(self):
+         """
+            nbd gb ;wirobgf kjhgvd
         """
-    nbd gb ;wirobgf kjhgvd
-    """
+         return self.__width
+
+     @width.setter
+     def width(self, value):
+        """
+            nbd gb ;wirobgf kjhgvd
+        """
         if not isinstance(value, int):
             raise TypeError("width must be an integer")
         if value <= 0:
             raise ValueError("width must be > 0")
         self.__width = value
 
-    @property
-    def height(self):
+     @property
+     def height(self):
         """
-    nbd gb ;wirobgf kjhgvd
-    """
+            nbd gb ;wirobgf kjhgvd
+        """
         return self.__height
 
-    @height.setter
-    def height(self, value):
+     @height.setter
+     def height(self, value):
         """
-    nbd gb ;wirobgf kjhgvd
-    """
+            nbd gb ;wirobgf kjhgvd
+        """
         if not isinstance(value, int):
             raise TypeError("height must be an integer")
         if value <= 0:
             raise ValueError("height must be > 0")
         self.__height = value
 
-    @property
-    def x(self):
+     @property
+     def x(self):
         """
-    nbd gb ;wirobgf kjhgvd
-    """
+            nbd gb ;wirobgf kjhgvd
+        """
         return self.__x
 
-    @x.setter
-    def x(self, value):
+     @x.setter
+     def x(self, value):
         """
-    nbd gb ;wirobgf kjhgvd
-    """
+            nbd gb ;wirobgf kjhgvd
+        """
         if not isinstance(value, int):
             raise TypeError("x must be an integer")
         if value < 0:
             raise ValueError("x must be >= 0")
         self.__x = value
 
-    @property
-    def y(self):
+     @property
+     def y(self):
         """
-    nbd gb ;wirobgf kjhgvd
-    """
+            nbd gb ;wirobgf kjhgvd
+        """
         return self.__y
 
-    @y.setter
-    def y(self, value):
+     @y.setter
+     def y(self, value):
         """
-    nbd gb ;wirobgf kjhgvd
-    """
+            nbd gb ;wirobgf kjhgvd
+        """
         if not isinstance(value, int):
             raise TypeError("y must be an integer")
         if value < 0:
             raise ValueError("y must be >= 0")
         self.__y = value
 
-    def area(self):
+     def area(self):
         """
-    nbd gb ;wirobgf kjhgvd
-    """
+            nbd gb ;wirobgf kjhgvd
+        """
         return self.__width * self.__height
 
-    def display(self):
+     def display(self):
         """
-    nbd gb ;wirobgf kjhgvd
-    """
+            nbd gb ;wirobgf kjhgvd
+        """
+        for _ in range(self.__y):
+            print()
         for _ in range(self.__height):
-            print("#" * self.__width)
+            print(" " * self.__x + "#" * self.__width)
+
+     def __str__(self):
+        """
+            nbd gb ;wirobgf kjhgvd
+        """
+        return "[Rectangle] ({}) {}/{} - {}/{}".format(self.id, self.__x, self.__y, self.__width, self.__height)
+
+     def update(self, *args):
+        """
+            nbd gb ;wirobgf kjhgvd
+        """
+        attributes = ['id', 'width', 'height', 'x', 'y']
+        for i, arg in enumerate(args):
+            setattr(self, attributes[i], arg)

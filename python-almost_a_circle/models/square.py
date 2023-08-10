358c2623890dcff8f2047c4784d5834c4a616a4e
1012lm <lm1012smile@gmail.com>
2023-08-10 00:16:20 -0800

    nbclov  ;lfv

--- python-almost_a_circle/models/square.py
+++ python-almost_a_circle/models/square.py
@@ -1,7 +1,6 @@
 """
  xc c b cv hfcv 
 """
-
 from models.rectangle import Rectangle
 
 class Square(Rectangle):
@@ -10,27 +9,27 @@ class Square(Rectangle):
     """
     def __init__(self, size, x=0, y=0, id=None):
         """
-        bxo bsd;xcb kjbc
-        """
+    bxo bsd;xcb kjbc
+    """
         super().__init__(size, size, x, y, id)
 
     @property
     def size(self):
         """
-        bxo bsd;xcb kjbc
-        """
+    bxo bsd;xcb kjbc
+    """
         return self.width
 
     @size.setter
     def size(self, value):
         """
-        bxo bsd;xcb kjbc
-        """
+    bxo bsd;xcb kjbc
+    """
         self.width = value
         self.height = value
 
     def __str__(self):
         """
-        bxo bsd;xcb kjbc
-        """
+    bxo bsd;xcb kjbc
+    """
         return "[Square] ({}) {}/{} - {}".format(self.id, self.x, self.y, self.width)

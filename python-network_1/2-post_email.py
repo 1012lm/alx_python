affa7542c2266544d4f691d3579ae044122e9dc2
1012lm <lm1012smile@gmail.com>
2023-08-10 00:44:51 -0800

    nwbf;v

---
+++ python-network_1/2-post_email.py
@@ -0,0 +1,20 @@
+"""
+    vcbldsvb ,c b
+"""
+
+import requests
+import sys
+"""
+    kbc; fhzbcv ;
+"""
+
+url = sys.argv[1]
+email = sys.argv[2]
+
+
+data = {'email': email}
+
+response = requests.post(url, data=data)
+
+if __name__ == "__main__":
+    print(response.text)

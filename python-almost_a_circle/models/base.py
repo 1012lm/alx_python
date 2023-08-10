95fa132560f3388e5aedfdf152507c342680c4d5
1012lm <lm1012smile@gmail.com>
2023-08-09 23:31:03 -0800

    nmvIDC

---
+++ python-almost_a_circle/models/base.py
@@ -0,0 +1,19 @@
+"""
+    almost circle project
+"""
+
+class Base:
+    """
+    base class
+    """
+    __nb_objects = 0
+
+    def __init__(self, id=None):
+        """
+            xbuidfcw\=cn kwdaosc        
+        """
+        if id is not None:
+            self.id = id
+        else:
+            Base.__nb_objects += 1
+            self.id = Base.__nb_objects

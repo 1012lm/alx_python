a603c93e8dc10e84eb922763c047a41d75f69f6d
1012lm <lm1012smile@gmail.com>
2023-08-10 13:03:46 -0800

    nbmv v,kli.

--- python-network_1/5-json_api.py
+++ python-network_1/5-json_api.py
@@ -9,6 +9,25 @@ import requests
 import sys
 
 
+def display_user_info(response):
+    """
+    Sends a request to the given URL and displays the body of the response.
+    If the HTTP status code is greater than or equal to 400, prints an error code.
+
+    Args:
+        url (str): The URL to send the request to.
+    """
+    try:
+        data = response.json()
+        if data:
+            user_info = f"[{data.get('id')}] {data.get('name')}"
+            print(user_info)
+        else:
+            print("No result")
+    except ValueError:
+        print("Not a valid JSON")
+
+
 def search_user(letter):
     """
     Sends a request to the given URL and displays the body of the response.
@@ -17,21 +36,14 @@ def search_user(letter):
     Args:
         url (str): The URL to send the request to.
     """
-    url = 'http://0.0.0.0:5000/search_user'
-    params = {'q': letter}
-    response = requests.post(url, params=params)
-
-    try:
-        data = response.json()
-        if isinstance(data, dict):
-            if 'id' in data and 'name' in data:
-                print(f"[{data['id']}] {data['name']}")
-            else:
-                print("No result")
-        else:
-            print("No result")
-    except ValueError:
-        print("Not a valid JSON")
+    url = "http://0.0.0.0:5000/search_user"
+    params = {"q": letter}
+    response = requests.post(url, data=params)
+    status_code = response.status_code
+    if status_code >= 400:
+        print(f"Error code: {status_code}")
+    else:
+        display_user_info(response)
 
 
 # Get the letter from the command-line argument

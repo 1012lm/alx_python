0b5fde942b5c5731dadeb2e24c4ed94495cff801
1012lm <lm1012smile@gmail.com>
2023-08-09 22:41:53 -0800

    bcidf

--- python-inheritance/6-rectangle.py
+++ python-inheritance/6-rectangle.py
@@ -1,43 +1,7 @@
 """
     The code snippet provided defines two classes: BaseGeometry and Rectangle.
 """
-
-class BaseGeometry:
-    """
-    A base class for geometrical operations.
-
-    Methods:
-        area(self): Raises an exception indicating that the method is not implemented.
-        integer_validator(self, name, value): Validates if a value is an integer greater than zero.
-    """
-    
-    def area(self):
-        """
-        Calculate the area of the geometry.
-
-        Raises:
-            Exception: Indicates that the method is not implemented.
-        """
-        raise Exception("area() is not implemented")
-
-    def integer_validator(self, name, value):
-        """
-        Validate if a value is an integer greater than zero.
-
-        Parameters:
-            name (str): The name of the value being validated.
-            value (int): The value to be validated.
-
-        Raises:
-            TypeError: If the value is not an integer.
-            ValueError: If the value is less than or equal to zero.
-        """
-        if not isinstance(value, int):
-            raise TypeError("{} must be an integer".format(name))
-        if value <= 0:
-            raise ValueError("{} must be greater than 0".format(name))
-
-
+from 5-base_geometry ; import BaseGeometry
 class Rectangle(BaseGeometry):
     """
     A class representing a rectangle.

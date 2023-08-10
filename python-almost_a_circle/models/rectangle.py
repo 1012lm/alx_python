ce693a1c398b9e8c1e1f35ace5e041b22dcd4134
1012lm <lm1012smile@gmail.com>
2023-08-09 23:44:22 -0800

    mnbdlq43

--- python-almost_a_circle/models/rectangle.py
+++ python-almost_a_circle/models/rectangle.py
@@ -1,17 +1,7 @@
-"""
-bxsuafvtbv=o kaednbfv ewsfdcxv;jn q
-"""
-
 from models.base import Base
 
 class Rectangle(Base):
-    """
-    cabd slfvo;awhnfsc alv 
-    """
     def __init__(self, width, height, x=0, y=0, id=None):
-        """
-         bd cvo;sfcqjwdbv z;cfcwsafcje]q[wse-dc]
-        """
         super().__init__(id)
         self.width = width
         self.height = height
@@ -20,56 +10,48 @@ class Rectangle(Base):
 
     @property
     def width(self):
-        """
-            mbdlxfiuval;rdfov
-        """
         return self.__width
 
     @width.setter
     def width(self, value):
-         """
-            mbdlxfiuval;rdfov
-        """
-         self.__width = value
+        if not isinstance(value, int):
+            raise TypeError("width must be an integer")
+        if value <= 0:
+            raise ValueError("width must be > 0")
+        self.__width = value
 
     @property
     def height(self):
-         """
-            mbdlxfiuval;rdfov
-        """
-         return self.__height
+        return self.__height
 
     @height.setter
     def height(self, value):
-         """
-            mbdlxfiuval;rdfov
-        """
-         self.__height = value
+        if not isinstance(value, int):
+            raise TypeError("height must be an integer")
+        if value <= 0:
+            raise ValueError("height must be > 0")
+        self.__height = value
 
     @property
     def x(self):
-         """
-            mbdlxfiuval;rdfov
-        """
-         return self.__x
+        return self.__x
 
     @x.setter
     def x(self, value):
-         """
-            mbdlxfiuval;rdfov
-        """
-         self.__x = value
+        if not isinstance(value, int):
+            raise TypeError("x must be an integer")
+        if value < 0:
+            raise ValueError("x must be >= 0")
+        self.__x = value
 
     @property
     def y(self):
-         """
-            mbdlxfiuval;rdfov
-        """
-         return self.__y
+        return self.__y
 
     @y.setter
     def y(self, value):
-         """
-            mbdlxfiuval;rdfov
-        """
-         self.__y = value
+        if not isinstance(value, int):
+            raise TypeError("y must be an integer")
+        if value < 0:
+            raise ValueError("y must be >= 0")
+        self.__y = value

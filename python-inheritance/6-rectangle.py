5278686d8bb772a7272e1a431cdb7f4caa30d5b5
1012lm <lm1012smile@gmail.com>
2023-08-09 22:24:27 -0800

    bfhesdf

--- python-inheritance/6-rectangle.py
+++ python-inheritance/6-rectangle.py
@@ -10,7 +10,13 @@ class BaseGeometry:
         area(self): Raises an exception indicating that the method is not implemented.
         integer_validator(self, name, value): Validates if a value is an integer greater than zero.
     """
-
+    def __dir__(Rectangle):
+        return [
+          '__class__', '__delattr__', '__dict__', '__dir__', '__doc__', '__eq__',
+            '__format__', '__ge__', '__getattribute__', '__gt__', '__hash__', '__init__',
+              '__le__', '__lt__', '__module__', '__ne__', '__new__', '__reduce__', '__reduce_ex__',
+                '__repr__', '__setattr__', '__sizeof__', '__str__', '__subclasshook__', '__weakref__', 'area', 'integer_validator'
+        ]
     def area(self):
         """
         Calculate the area of the geometry.

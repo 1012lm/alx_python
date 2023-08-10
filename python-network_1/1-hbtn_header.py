b8f8fca0dee109b6d12c7930ff3544b2276668d1
1012lm <lm1012smile@gmail.com>
2023-08-10 00:33:25 -0800

    nbf'v

--- python-network_1/1-hbtn_header.py
+++ python-network_1/1-hbtn_header.py
@@ -1,16 +1,26 @@
-# Import the requests module for making HTTP requests
+"""
+    Import the requests module for making HTTP requests
+"""
 import requests
 import sys
 
-# Get the URL from the command-line argument
+"""
+    Get the URL from the command-line argument
+"""
 url = sys.argv[1]
 
-# Send a GET request to the specified URL
+"""
+    Send a GET request to the specified URL
+"""
 response = requests.get(url)
 
-# Get the value of the X-Request-Id header from the response
+"""
+    Get the value of the X-Request-Id header from the response
+"""
 request_id = response.headers.get('X-Request-Id')
 
-# Print the value of the X-Request-Id header
+"""
+    Print the value of the X-Request-Id header
+"""
 if __name__ == "__main__":
     print(request_id)

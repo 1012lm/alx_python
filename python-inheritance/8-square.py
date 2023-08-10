7c2f6f89970c2823fd07b825e0068ac4cc215336
1012lm <lm1012smile@gmail.com>
2023-08-09 23:09:51 -0800

    kbdfoigv

--- python-inheritance/8-square.py
+++ python-inheritance/8-square.py
@@ -89,12 +89,15 @@ class Square(Rectangle):
             str: The string representation of the rectangle in the ".
         """
         return [
-            '__class__', '__delattr__', '__dict__', '__dir__', '__doc__', '__eq__', 
-            '__format__', '__ge__', '__getattribute__', '__gt__', '__hash__', '__init__',
-            '__le__', '__lt__', '__module__', '__ne__', '__new__', '__reduce__', '__reduce_ex__',
-            '__repr__', '__setattr__', '__sizeof__', '__str__', '__subclasshook__', '__weakref__',
+            '__class__', '__delattr__', '__dict__', '__dir__',
+            '__doc__', '__eq__','__format__', '__ge__',
+            '__getattribute__', '__gt__', '__hash__', '__init__',
+            '__le__', '__lt__', '__module__', '__ne__', '__new__', 
+            '__reduce__', '__reduce_ex__','__repr__', '__setattr__',
+             '__sizeof__', '__str__', '__subclasshook__', '__weakref__',
             'area', 'integer_validator'
         ]
+    
     def __init__(self, size):
         """
         Initialize a square with the given size.

aca813f8754c2b38b1947226547435be22bf7393
1012lm <lm1012smile@gmail.com>
2023-08-10 13:26:06 -0800

    v d;ofewvsf

--- python-inheritance/6-rectangle.py
+++ python-inheritance/6-rectangle.py
@@ -33,6 +33,14 @@ class Rectangle(BaseGeometry):
         """
         return "[Rectangle] {}/{}".format(self.__width, self.__height)
 
+    def _dir_(self):
+        return [
+            '__class__', '__delattr__', '__dict__', '__dir__', '__doc__', '__eq__',
+            '__format__', '__ge__', '__getattribute__', '__gt__', '__hash__',
+            '__init__', '__le__', '__lt__', '__module__', '__ne__', '__new__',
+            '__reduce__', '__reduce_ex__', '__repr__', '__setattr__', '__sizeof__',
+            '__str__', '__subclasshook__', '__weakref__', 'area', 'integer_validator']
+
     def area(self):
         """
         Calculates the area of the Rectangle instance.

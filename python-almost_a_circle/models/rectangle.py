39ae40cfd5e8e10f966abbb5210df15e94f33e06
1012lm <lm1012smile@gmail.com>
2023-08-09 23:38:25 -0800

    sdlca

--- python-almost_a_circle/models/rectangle.py
+++ python-almost_a_circle/models/rectangle.py
@@ -1,7 +1,17 @@
+"""
+bxsuafvtbv=o kaednbfv ewsfdcxv;jn q
+"""
+
 from models.base import Base
 
 class Rectangle(Base):
+    """
+    cabd slfvo;awhnfsc alv 
+    """
     def __init__(self, width, height, x=0, y=0, id=None):
+        """
+         bd cvo;sfcqjwdbv z;cfcwsafcje]q[wse-dc]
+        """
         super().__init__(id)
         self.width = width
         self.height = height
@@ -10,32 +20,56 @@ class Rectangle(Base):
 
     @property
     def width(self):
+        """
+            mbdlxfiuval;rdfov
+        """
         return self.__width
 
     @width.setter
     def width(self, value):
-        self.__width = value
+         """
+            mbdlxfiuval;rdfov
+        """
+         self.__width = value
 
     @property
     def height(self):
-        return self.__height
+         """
+            mbdlxfiuval;rdfov
+        """
+         return self.__height
 
     @height.setter
     def height(self, value):
-        self.__height = value
+         """
+            mbdlxfiuval;rdfov
+        """
+         self.__height = value
 
     @property
     def x(self):
-        return self.__x
+         """
+            mbdlxfiuval;rdfov
+        """
+         return self.__x
 
     @x.setter
     def x(self, value):
-        self.__x = value
+         """
+            mbdlxfiuval;rdfov
+        """
+         self.__x = value
 
     @property
     def y(self):
-        return self.__y
+         """
+            mbdlxfiuval;rdfov
+        """
+         return self.__y
 
     @y.setter
     def y(self, value):
-        self.__y = value
+         """
+            mbdlxfiuval;rdfov
+        """
+         self.__y = value

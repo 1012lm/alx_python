9fab9992c0b115678fd87c9e2b48c2e7409f2f79
1012lm <lm1012smile@gmail.com>
2023-08-23 22:16:46 -0800

    task 5

---
+++ python-object_relational_mapping/5-filter_cities.py
@@ -0,0 +1,47 @@
+#!/usr/bin/python3
+"""
+Script that lists all cities of a given state from the database hbtn_0e_4_usa.
+"""
+
+import sys
+import MySQLdb
+
+if __name__ == '__main__':
+    # Get MySQL credentials, database name, and state name from command-line
+    username = sys.argv[1]
+    password = sys.argv[2]
+    database = sys.argv[3]
+    state_name = sys.argv[4]
+
+    # Connect to MySQL server
+    db = MySQLdb.connect(
+        host='localhost',
+        port=3306,
+        user=username,
+        passwd=password,
+        db=database
+    )
+
+    # Create a cursor object to execute SQL queries
+    cur = db.cursor()
+
+    # Execute the query to fetch cities of the given state sorted by cities.id
+    query = """
+    SELECT GROUP_CONCAT(cities.name SEPARATOR ', ')
+    FROM cities
+    JOIN states ON cities.state_id = states.id
+    WHERE states.name = %s
+    ORDER BY cities.id ASC
+    """
+    cur.execute(query, (state_name,))
+
+    # Fetch the result
+    result = cur.fetchone()[0]
+
+    # Print the cities of the given state
+    if result:
+        print(result)
+
+    # Close the cursor and database connection
+    cur.close()
+    db.close()

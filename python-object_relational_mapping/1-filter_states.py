731f7659c87045d61e718ce1e3ecc61fae817d0d
1012lm <lm1012smile@gmail.com>
2023-08-23 20:46:01 -0800

    task 1

--- python-object_relational_mapping/1-filter_states.py
+++ python-object_relational_mapping/1-filter_states.py
@@ -1,5 +1,6 @@
 """
-Script that lists all states with a name starting with N (upper N) from the database hbtn_0e_0_usa.
+Script that lists all states with a name starting with N (upper N)
+from the database hbtn_0e_0_usa.
 """
 
 import sys

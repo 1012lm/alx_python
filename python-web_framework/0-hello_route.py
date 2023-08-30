724ae801001886eb963c05b7c3459b8fbe3c9d1b
1012lm <lm1012smile@gmail.com>
2023-08-30 10:48:26 -0800

    hello route

---
+++ python-web_framework/0-hello_route.py
@@ -0,0 +1,19 @@
+"""
+This is a simple Flask web application that displays 'Hello HBNB!' when accessed.
+"""
+
+from flask import Flask
+
+app = Flask(__name__)
+
+
+@app.route('/', strict_slashes=False)
+def hello_hbnb():
+    """
+    Route handler for the root URL.
+    """
+    return 'Hello HBNB!'
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
